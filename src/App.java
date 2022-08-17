090c883e4ea00d17a1803dfe97a2a679d841b666
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-17 14:32:42 +0800

    changed x to y

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ public class App {
 
     static int addNum()
     {
-        int x = 5;
-        return x;
+        int y = 5;
+        return y;
     }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!" + addNum());
