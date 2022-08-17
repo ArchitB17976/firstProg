9060bd44af3b01cab6d8fa3e4a5daad4ce48ff2d
ArchitB17976 <44702605+ArchitB17976@users.noreply.github.com>
2022-08-17 14:30:21 +0800

    added a static function

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
+
+    static int addNum()
+    {
+        int x = 5;
+        return x;
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!" + addNum());
     }
 }
